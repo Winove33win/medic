16e30d5091937af704f8bd6a756d412fd044f1e6
Winove <fernando@winove.com.br>
2025-11-24 15:45:47 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, (process as any).cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      // Define process.env to an empty object to prevent "process is not defined" errors in browser
+      'process.env': {}
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    }
+  };
 });
